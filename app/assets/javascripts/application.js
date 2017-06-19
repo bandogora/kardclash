43f766fc109e614c7b51b65e448e7709f5442ccf
Justin <juforgue@gmail.com>
2017-06-19 16:05:56 -0400

    major style revisions, added sessions

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,6 @@
 //
 //= require jquery
 //= require rails-ujs
-//= require bootstrap-sprockets
+//= require bootstrap/dist/js/bootstrap
 //= require turbolinks
 //= require_tree .
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,21 @@ class SessionsController < ApplicationController
   end
 
   def create
-    render 'pages/home'
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = "You're logged in!"
+      redirect_to user_path(user)
+    else
+      flash.now[:danger] = 'There was an issue with your login info.'
+      render 'new'
+    end
   end
 
   def destroy
-
+    session[:user_id] = nil
+    flash[:success] = "You're logged out."
+    redirect_to root_path
   end
 
 end
